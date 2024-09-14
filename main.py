9a0bf807e6d6823811f1cf458022c5d9b4aa65b1
frstrtr <frstrtr@gmail.com>
2024-09-14 12:54:16 +0400

    fromatting update

--- main.py
+++ main.py
@@ -856,13 +856,13 @@ async def save_inout_event(update: types.ChatMemberUpdated, lols_spam):
     """Function to record user join/leave events."""
 
     event_record = (
-        f"{'💀 ' if lols_spam else '😊 '}"
         f"{datetime.now().strftime('%H:%M:%S.%f')}: "  # Date and time with milliseconds
         f"{update.old_chat_member.user.id}:"
+        f"{'💀 ' if lols_spam else '😊 '}"
         f"{' '.join(f'@{getattr(update.old_chat_member.user, attr)}' if attr == 'username' else str(getattr(update.old_chat_member.user, attr, '')) for attr in ('username', 'first_name', 'last_name') if getattr(update.old_chat_member.user, attr, ''))} "
         f"{update.old_chat_member.status} --> {update.new_chat_member.status} in {'@' + update.chat.username + ': ' if update.chat.username else ''}{update.chat.title} by "
         f"{update.from_user.id}:"
-        f"{' '.join(f'@{getattr(update.from_user, attr)}' if attr == 'username' else str(getattr(update.from_user, attr, '')) for attr in ('username', 'first_name', 'last_name') if getattr(update.from_user, attr, ''))}"
+        f"{' '.join(f'@{getattr(update.from_user, attr)}' if attr == 'username' else str(getattr(update.from_user, attr, '')) for attr in ('username', 'first_name', 'last_name') if getattr(update.from_user, attr, ''))}\n"
     )
 
     LOGGER.debug("Event record: %s", event_record)

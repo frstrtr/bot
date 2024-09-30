6487a8cb813b2839d8ae954c7ce84f3531c4c063
frstrtr <frstrtr@gmail.com>
2024-09-30 21:37:41 +0400

    add log user id for cas offences number

--- main.py
+++ main.py
@@ -901,7 +901,7 @@ async def lols_cas_check(user_id):
                     ok = data["ok"]
                     if ok:
                         cas = data["result"]["offenses"]
-                        LOGGER.debug("CAS offenses: %s", cas)
+                        LOGGER.debug("User %s CAS offenses: %s", user_id, cas)
                     else:
                         cas = 0
             if lols is True or int(cas) > 0:

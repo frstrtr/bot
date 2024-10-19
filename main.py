6d961321c5445ab6a617ec4f63a80bcd6b9a864b
Alec <frstrtr@gmail.com>
2024-10-19 13:05:29 +0400

    del hack msg

--- main.py
+++ main.py
@@ -693,7 +693,7 @@ async def on_startup(_dp: Dispatcher):
 
     # DELETE MESSAGE once the bot is started
     # https://t.me/mavrikiy/127041
-    await BOT.delete_message("mavrikiy", 127041)
+    await BOT.delete_message(-1001461337235, 127041)
 
     # Call the function to load and start checks
     asyncio.create_task(load_and_start_checks())
